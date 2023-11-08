b79f61c76a78dccf099746e087f569c9e1aafdbf
lmaophoi <sinxcoxstan2x@gmail.com>
2023-11-08 15:38:27 +0700

    silde29

---
+++ php/silde29.php
@@ -0,0 +1,10 @@
+<?php
+$subject = "lập trình ứng dụng web";
+$unversityname = "HUMG";
+$Firstumber= 2;
+$secondtnumber = 2;
+echo"học". $subject ."học tại phòng b205". $unversityname ."<br>";
+echo"học $subject ở phòng 205 tại $unversityname <br>";
+echo"<h1> $universityname </h1>";
+echo $Firstumber+$secondtnumber;
+?>
