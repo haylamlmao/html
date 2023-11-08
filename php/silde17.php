d48a8d82d0c446466c704298850a24e55163fa1f
lmaophoi <sinxcoxstan2x@gmail.com>
2023-11-08 15:08:50 +0700

    slide17

---
+++ php/silde17.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>trương đình văn </title>
+</head>
+<body>
+    <?php
+    echo"my first php file";
+    echo"hello world";
+    echo"<select>";
+    echo"<option>1</option>";
+    echo"<option>2</option>";
+    ?>
+</body>
+</html>
