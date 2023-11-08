49de47690e94ec4565a542dbd01e53f26cb304c6
lmaophoi <sinxcoxstan2x@gmail.com>
2023-11-08 15:58:30 +0700

    silde36

---
+++ php/silde34.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>trương đình văn 2121051389</title>
+</head>
+<body>
+    <form action="5-formGet.php" method="Get">
+        <p>name: </p>
+        <input type="text" name="name" id="name">
+        <p>e - mail :</p>
+        <input type="text" name="email" id="email"><br>
+        <p></p>
+        <input type="submit">
+    </form>
+</body>
+</html>
