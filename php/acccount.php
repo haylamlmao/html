88ea87f11bb804bf5155cda6dea753125412a78b
lmaophoi <sinxcoxstan2x@gmail.com>
2023-11-08 22:16:51 +0700

    silde52

---
+++ php/acccount.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>trương đình văn 2121051389</title>
+</head>
+<body>
+     <?php echo $_GET["name"]; #em không tìm được cách để đưa cả 2 loại method vào cũng 1 form hay submit cùng 1 lúc ạ ?><br>
+     <?php echo $_GET["pass"] ?><br>
+     <?php echo $_POST["email"]?><br>
+     <?php echo $_POST["diachi"] ?> 
+</body>
+</html>
