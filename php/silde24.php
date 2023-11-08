edd3e97eb9c6b84a91511512745f59b2dbdc018a
lmaophoi <sinxcoxstan2x@gmail.com>
2023-11-08 15:22:03 +0700

    slide24

---
+++ php/silde24.php
@@ -0,0 +1,9 @@
+<?php
+$longacccount = 10.000.000;
+echo"số tiền trong tai khoang long là : $longacccount ";
+$humgpay = 10.000.000;
+echo"humg chuyển tiền : $humgpay";
+$longacccount = $longacccount+$humgpay;
+echo"số tiền trong tài khoản long : $longacccount";
+#trương đình văn 2121051389
+?>
