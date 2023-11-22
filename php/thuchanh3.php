ab0e4e25c176af98d40025f6e577b5299de69f7f
lmaophoi <sinxcoxstan2x@gmail.com>
2023-11-22 15:47:29 +0700

    phpthuchanh3

---
+++ php/thuchanh3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>trương đình văn 2121051389</title>
+</head>
+<body>
+    <?php
+    $cntt = 18;
+    $cnttcls = 22;
+    $khdl = 18;
+    $diachat = 17;
+    $moitruong = 15;
+    if ($_SERVER["REQUEST_METHOD"]=="POST"){
+        $toan = $_POST["toan"];
+        $ly = $_POST["ly"];
+        $hoa = $_POST["hoa"];
+        $diemtb = ($toan + $ly + $hoa)/3;
+        if $diemtb <$cntt{
+            echo"lmao";
+        }
+    }
+    ?>
+</body>
+</html>
