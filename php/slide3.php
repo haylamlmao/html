bf85255d14cdf6e1b9aff6a3f417ea89ceef1f9d
lmaophoi <sinxcoxstan2x@gmail.com>
2023-11-22 14:51:45 +0700

    slide2

---
+++ php/slide3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $t = true;
+    $f = false;
+    echo "t = $t <br>";
+    echo "f = $f <br>";
+    $check1 = 10>5;
+    $check2 = 20<10;
+    $check3 = $check1 && $check2;
+    $check4 = $check1 || $check2;
+    echo "check1 =$check1 <br>";
+    echo "check2 = $check2 <br>";
+    echo "check3 = $check3 <br>";
+    echo "check4 = $check4 <br>";
+    /// trương đình văn 2121051389
+    ?>
+</body>
+</html>
