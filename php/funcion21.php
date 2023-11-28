41f9a1c5d779725ca6fed8f894385a4c06943340
lmaophoi <sinxcoxstan2x@gmail.com>
2023-11-28 19:18:35 +0700

    silde

---
+++ php/funcion21.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>trương đình văn 2121051389</title>
+</head>
+<body>
+    <?php 
+    function display_img($imgalist){
+        foreach ($imgalist as $item)
+        {
+            echo "<img src='images/$item
+        }
+    }
+    ?>
+</body>
+</html>
