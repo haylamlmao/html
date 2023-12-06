955a1057aabac2d8dbb68cc7c01bf2e2a05e6330
lmaophoi <sinxcoxstan2x@gmail.com>
2023-12-06 14:51:11 +0700

    insertdata

---
+++ php/slide45.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>trương đình văn 2121051389</title>
+</head>
+<body>
+    <?php
+    require 'silde42.php';
+    $sql= "INSERT INTO dskhachhang (ghengoi,name,diachi,number) values (1,"trương đình văn","khánh hồng","0366")";
+    $conn->query($sql);
+    $conn->close();
+
+    ?>
+</body>
+</html>
